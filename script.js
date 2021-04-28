ac69c6b28e0c636591e865069f8aa06e4c1676e7
Jing Wang <wjing.archdesi@gmail.com>
2021-04-28 00:14:30 -0400

    modified media query for smaller devices and modified js

--- script.js
+++ script.js
@@ -1,37 +1,50 @@
-let currentSlideIndex = 0;
-const slides = document.getElementsByClassName('subtext');
+const proEl = document.getElementById("profession");
+const firstEl = proEl.firstElementChild;
+const lastEl = proEl.lastElementChild;
+const professions = ["Graphic Designer", "Motion Graphics", "3D Modeling"];
+let professionsIdx = 0;
+let firstWord = "";
+let firstWordIndex = 0;
+let secondWord = "";
+let secondWordIndex = 0;
+
+setNextWord();
+showNextLetter();
 
 setInterval(() => {
-    nextSlide();
-}, 3000);
+    showNextLetter();
+}, 500);
 
-function prevSlide() {
-    currentSlideIndex--;
-    currentSlideIndex =
-        currentSlideIndex < 0 ? slides.length - 1 : currentSlideIndex;
-    showSlide();
+function setNextWord() {
+    const words = professions[professionsIdx].split(" ");
+    firstWord = words[0];
+    secondWord = words[1];
+    firstWordIndex = 0;
+    secondWordIndex = 0;
 }
 
-function nextSlide() {
-    currentSlideIndex++;
-    currentSlideIndex =
-        currentSlideIndex >= slides.length ? 0 : currentSlideIndex;
-    showSlide();
-}
-
-function showSlide() {
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none';
+function showNextLetter() {
+    if (firstWordIndex <= firstWord.length) {
+        firstEl.innerText = firstWord.substr(0, firstWordIndex);
+        firstWordIndex++;
+    } else if (secondWordIndex <= secondWord.length) {
+        lastEl.innerText = secondWord.substr(0, secondWordIndex);
+        secondWordIndex++;
+    } else {
+        ++professionsIdx;
+        professionsIdx =
+            professionsIdx >= professions.length ? 0 : professionsIdx;
+        setNextWord();
+        firstEl.innerText = "";
+        lastEl.innerText = "";
     }
-    slides[currentSlideIndex].style.display = 'block';
 }
-
 
 function showResponsiveNav() {
     const x = document.getElementById("topnav");
     if (x.className === "topnav") {
-      x.className += " responsive";
+        x.className += " responsive";
     } else {
-      x.className = "topnav";
+        x.className = "topnav";
     }
-  }
+}
